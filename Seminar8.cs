a92790d6fa07659e1363d462c05798b057db1f2e
Semion Volkov <semen_volkov@mail.ru>
2023-01-05 17:50:44 +0500

    Task3 added

--- Seminar8.cs
+++ Seminar8.cs
@@ -99,4 +99,33 @@ void MinRow(int[,] lowMatr)
     Console.WriteLine($"{lowestRowNumber} строка");
 }
 
-MinRow(IntMatrix(4, 4));
+//MinRow(IntMatrix(4, 4));
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+void MatrixProduct(int[,] matrixA, int[,] matrixB)
+{
+    int[,] productedMatrix = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+    for (int i = 0; i < productedMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < productedMatrix.GetLength(1); j++)
+        {
+            int elementIJ = 0;
+            for(int k = 0; k < matrixA.GetLength(1); k++)
+            {
+                elementIJ += matrixA[i, k] * matrixB [k, j];
+            }
+            productedMatrix[i,j] = elementIJ;
+        }
+    } 
+    ShowMatrix(productedMatrix);
+}
+
+MatrixProduct(IntMatrix(3, 2), IntMatrix(2, 3));
