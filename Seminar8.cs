8ae527fbd160d6ae30b242b3c24d9b2814b37c51
Semion Volkov <semen_volkov@mail.ru>
2023-01-06 16:00:01 +0500

    Task5 added

--- Seminar8.cs
+++ Seminar8.cs
@@ -187,4 +187,53 @@ void Array3d(int m, int n, int o)
     
 }
 
-Array3d(2, 2, 2);
+//Array3d(2, 2, 2);
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+void SpiralArray(int m, int n)
+{
+    int[,] spArr = new int[m,n];
+
+    int row = 0;
+    int col = 0;
+    int x = 1;
+    int y = 0;
+    int direction = 0;
+    int lim = n;
+
+    for (int k = 0; k < spArr.Length; k++)
+    {
+        spArr[row, col] = k + 1;
+        lim--;
+
+        if (lim == 0)
+        {
+            lim = n*(direction % 2) + m*((direction + 1)%2) - (direction/2 - 1) - 2;
+            int temp = x;
+            x = -y;
+            y = temp;
+            direction++;
+        }
+
+        col += x;
+        row += y;
+    }
+
+    for(int i = 0; i < spArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < spArr.GetLength(1); j++)
+        {
+            Console.Write($"{spArr[i, j].ToString("00")} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+SpiralArray(4, 4);
